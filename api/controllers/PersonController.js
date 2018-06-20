61daaeef7c34ae09dd3c218dfc220843fcc53f33
AlbertoSdlF <albertosaezdelafuente@gmail.com>
2018-06-20 12:38:21 +0100

    Person API

---
+++ api/controllers/PersonController.js
@@ -0,0 +1,21 @@
+/**
+ * PersonController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+  /**
+   * `PersonController.first_name()`
+   */
+  first_name: async function (req, res) {
+    return res.json({
+      todo: 'first_name() is not implemented yet!'
+    });
+  }
+
+};
+
